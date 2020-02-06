1d3140b122c65055f68d8f77f8d5ad0a77c00a5b
Twesty <xomu4557@gmail.com>
2020-02-06 23:28:53 +0200

    Mmicro changes

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	$(() => {
+	if (window.matchMedia("(min-width: 576px)").matches) {
+		
 		$('.latestPhotos__slides').slick({
 			slidesToShow: 3,
 			slidesToScroll: 1,
@@ -14,39 +15,33 @@ document.addEventListener("DOMContentLoaded", function() {
 					slidesToShow: 2
 				}
 				},
-				{
-				breakpoint: 480,
-				settings: {
-					slidesToShow: 1
-				}
-				}
 			]
 		});
 
 		$('.latestPhotos__bars').slick({
-			slidesToShow: 5,
+			slidesToShow: 4,
 			asNavFor: $('.latestPhotos__slides'),
 			centerMode: true,
 			focusOnSelect: true
 		});
 
-		$('.latestNews__slides').slick({
-			fade: true,
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			asNavFor: $('.latestNews__bars'),
-			nextArrow: $('.latestNews__next'),
-			prevArrow: $('.latestNews__prev'),
-		})
+	}
 
-		$('.latestNews__bars').slick({
-			slidesToShow: 3,
-			asNavFor: $('.latestNews__slides'),
-			centerMode: true,
-			focusOnSelect: true
-		});
-	
+	$('.latestNews__slides').slick({
+		fade: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		asNavFor: $('.latestNews__bars'),
+		nextArrow: $('.latestNews__next'),
+		prevArrow: $('.latestNews__prev'),
 	})
+
+	$('.latestNews__bars').slick({
+		slidesToShow: 3,
+		asNavFor: $('.latestNews__slides'),
+		centerMode: true,
+		focusOnSelect: true
+	});
 
 	$('.headerMenu__mobileBars').on('click', function(){
 
